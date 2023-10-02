685bd1bc677caaa9601e0079b9c60bb97beff748
FarhanMawaludin <farhanmawaludin7@gmail.com>
2023-10-02 12:27:15 +0700

    percobaan 2

---
+++ Pemilihan2Percobaan211.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan211 {
+    public static void main(String[] args) {
+        Scanner input11 = new Scanner(System.in);
+        float sudut1, sudut2,sudut3, totalSudut;
+
+        System.out.print("Masukan Sudut 1 : ");
+        sudut1 = input11.nextFloat();
+        System.out.print("Masukan Sudut 2 : ");
+        sudut2 = input11.nextFloat();
+        System.out.print("Masukan Sudut 3 : ");
+        sudut3 = input11.nextFloat();
+
+        totalSudut = sudut1 + sudut2 + sudut3;
+
+        if(totalSudut == 180) {
+            if((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) 
+            System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+            else 
+            System.out.println("Segitiga tersebut bukan segitiga siku-siku");
+            
+        }
+        else {
+            System.out.println("Bukan Segitiga");
+        }
+    }
+}
