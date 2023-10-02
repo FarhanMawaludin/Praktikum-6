efa1e0f05147c0746c22313e6eaf8400655ba96d
FarhanMawaludin <farhanmawaludin7@gmail.com>
2023-10-02 11:36:47 +0700

    modifikasi 2

--- Pemilihan2Percobaan111.java
+++ Pemilihan2Percobaan111.java
@@ -7,12 +7,8 @@ public class Pemilihan2Percobaan111 {
         System.out.print("Masukkan Tahun : ");
         int tahun = input11.nextInt();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0 || (tahun % 400) == 0) {
-                System.out.println("Tahun Kabisat");
-            } else {
-                System.out.println("Bukan Tahun Kabisat");
-            }
+        if ((tahun % 4) == 0 && (!(tahun % 100 == 0) || (tahun % 400 == 0))) {
+            System.out.println("Tahun Kabisat");
         } else {
             System.out.println("Bukan Tahun Kabisat");
         }
